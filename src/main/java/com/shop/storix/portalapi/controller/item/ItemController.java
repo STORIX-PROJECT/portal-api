827b02ac5eecfce82a3814edb48e185b1b2eb283
박현우 <simuoo96@gmail.com>
2026-03-12 17:25:27 +0900

    feat : 상품 검색 기능 로그 및 예외 처리 추가

--- src/main/java/com/shop/storix/portalapi/controller/item/ItemController.java
+++ src/main/java/com/shop/storix/portalapi/controller/item/ItemController.java
@@ -33,4 +33,5 @@ public class ItemController {
         return ApiResponse.ok(searchList);
     }
 
+
 }
--- src/main/java/com/shop/storix/portalapi/mapper/item/ItemMapper.java
+++ src/main/java/com/shop/storix/portalapi/mapper/item/ItemMapper.java
@@ -2,21 +2,11 @@ package com.shop.storix.portalapi.mapper.item;
 
 
 import com.shop.storix.portalapi.model.dto.item.request.ItemSearchRequest;
-import com.shop.storix.portalapi.model.dto.item.request.OptionValidRequest;
-import com.shop.storix.portalapi.model.dto.item.response.detail.ItemDetailDto;
-import com.shop.storix.portalapi.model.dto.item.response.option.ItemOptionDto;
 import com.shop.storix.portalapi.model.dto.item.response.search.ItemSearchDto;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 @Mapper
 public interface ItemMapper {
     List<ItemSearchDto.ItemSearchResponse> searchItem(ItemSearchRequest request);
-//    List<ItemSearchDto.ItemSearchResponse> searchCategory(CategorySearchRequest request);
-//    ItemDetailDto.ItemDetailResponse detailItem(Long itemNo);
-//    List<String> imgUrls(Long itemNo);
-//    List<ItemOptionDto.ItemOptionDetailResponse> optionItem(Long itemNo);
-//    ItemOptionDto.OptionCombiResponse findCombin(@Param("itemNo") Long itemNo, @Param("optionNos") List<Long> optionNos, @Param("optionCount") int optionCount);
 }
