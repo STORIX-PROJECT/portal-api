bd79260c0fa37c499e5bc701438e5dba49592029
박현우 <simuoo96@gmail.com>
2026-03-16 20:10:00 +0900

    feat: 위시리스트 해제 기능 분리 및 리팩토링

--- src/main/java/com/shop/storix/portalapi/controller/item/ItemWishController.java
+++ src/main/java/com/shop/storix/portalapi/controller/item/ItemWishController.java
@@ -1,6 +1,6 @@
 package com.shop.storix.portalapi.controller.item;
 
-import com.shop.storix.portalapi.controller.admin.ApiResponse;
+import com.shop.storix.portalapi.common.ApiResponse;
 import com.shop.storix.portalapi.model.dto.item.request.ItemWishRequestDto;
 import com.shop.storix.portalapi.service.item.ItemWishService;
 import io.swagger.v3.oas.annotations.Operation;
