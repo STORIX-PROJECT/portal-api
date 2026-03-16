b5da226bfb738e32c88072eafc9971ddbdf4d92d
박현우 <simuoo96@gmail.com>
2026-03-16 20:24:54 +0900

    feat: 관련 상품 기능 분리 및 리팩토링

--- src/main/java/com/shop/storix/portalapi/service/item/ItemRelateService.java
+++ src/main/java/com/shop/storix/portalapi/service/item/ItemRelateService.java
@@ -3,9 +3,8 @@ package com.shop.storix.portalapi.service.item;
 import com.shop.storix.portalapi.model.dto.item.response.relate.RelateItemDto;
 import org.apache.ibatis.annotations.Param;
 
-
 import java.util.List;
 
-public interface ItemSearchService {
+public interface ItemRelateService {
     List<RelateItemDto.RelateItemResponse> relateItem(@Param("itemNo") Long itemNo);
 }
